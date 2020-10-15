2d768535f048fc2fd372ec3f5e42f2f4087edc8b
Jiri Praus <jpraus@samepage.io>
2020-10-16 00:39:22 +0200

    planter install script to RPI

--- src/tools/planter-rpi/install.sh
+++ src/tools/planter-rpi/install.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
+
 sudo apt-get -y install python3 python3-pip
 sudo pip3 install RPi.GPIO Adafruit-CharLCD pyserial esptool
+
+touch planter-starter.sh
+chmod +x planter-starter.sh
+
+echo "#!/bin/bash" > planter-starter.sh
+echo "" >> planter-starter.sh
+echo "export PATH=\"$PATH\"" >> planter-starter.sh
+echo "cd `pwd`" >> planter-starter.sh
+echo "python3 planter.py &" >> planter-starter.sh
+
+(crontab -l 2>/dev/null; echo "@reboot bash `pwd`/planter-starter.sh") | crontab -
