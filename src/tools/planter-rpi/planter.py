7624bde347dd9a25d23f8e5ca33deb75c585800f
Jiri Praus <jpraus@samepage.io>
2020-10-16 11:58:08 +0200

    planter - auto-update test

--- src/tools/planter-rpi/planter.py
+++ src/tools/planter-rpi/planter.py
@@ -7,7 +7,7 @@ import serial.tools.list_ports as prtlst
 import os
 from time import sleep
 
-VERSION = 3
+VERSION = 4
 
 # Raspberry Pi pin configuration:
 lcd_rs        = 7  # Note this might need to be changed to 21 for older revision Pi's.
