6f90283b00d92a074de5d1625b00565b549bcfe8
Jiri Praus <jpraus@samepage.io>
2020-11-18 22:30:27 +0100

    planter - run planter as pi user

--- src/tools/planter-rpi/install.sh
+++ src/tools/planter-rpi/install.sh
@@ -14,7 +14,6 @@ chmod +x planter-starter.sh
 
 echo "#!/bin/bash" > planter-starter.sh
 echo "" >> planter-starter.sh
-echo "sudo su pi" >> planter-starter.sh
 echo "export PATH=\"$PATH\"" >> planter-starter.sh
 echo "cd `pwd`" >> planter-starter.sh
 echo "bash update.sh" >> planter-starter.sh
@@ -23,10 +22,10 @@ echo "python3 planter.py" >> planter-starter.sh
 if ! grep -q planter-starter /etc/rc.local
 then
 	echo "Adding to rc.local"
-	sudo sed -i -e '$i \bash /home/pi/floower/src/tools/planter-rpi/planter-starter.sh &\n' /etc/rc.local
+	sudo sed -i -e '$i \su pi -c "bash /home/pi/floower/src/tools/planter-rpi/planter-starter.sh &"\n' /etc/rc.local
 else
 	echo "Replacing to rc.local"
-	sudo sed -i '/planter-starter/c\bash /home/pi/floower/src/tools/planter-rpi/planter-starter.sh &' /etc/rc.local
+	sudo sed -i '/planter-starter/c\su pi -c "bash /home/pi/floower/src/tools/planter-rpi/planter-starter.sh &"' /etc/rc.local
 fi
 
 echo "Done"
